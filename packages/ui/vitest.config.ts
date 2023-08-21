dad8abb71f087645f93121d0a438dbae888c9e3e
Halis Yücel <isaycel2001@gmail.com>
2023-08-22 02:48:25 +0300

    chore(ui): configure vitest

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	test: {
-		environment: 'jsdom',
-		setupFiles: ['./src/setup.ts'],
-		globals: true
-	}
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/setup.ts'],
+    globals: true,
+  },
 });
