07e9a94e10c3c772ce8fa030e09540a63224cd3c
Halis Yücel <isaycel2001@gmail.com>
2023-09-23 13:45:58 +0300

    chore(ui): export ui component types

--- packages/ui/src/core/index.ts
+++ packages/ui/src/core/index.ts
@@ -1,9 +1,18 @@
-// Global styles
-import '@monorepo/styles/globals.scss';
-
 // Box
 export { default as Box } from './Box';
 
 // Text
 export { default as Text } from './Text';
 export type { TextProps } from './Text';
+
+// Flex
+export { default as Flex } from './Flex';
+export type { FlexProps } from './Flex';
+
+// Paper
+export { default as Paper } from './Paper';
+export type { PaperProps } from './Paper';
+
+// Icon
+export { default as Icon } from './Icon';
+export type { IconProps } from './Icon';
