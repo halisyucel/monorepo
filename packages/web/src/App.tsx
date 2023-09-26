835a2f85200ac2327d82ad0fd755cb0af8da7f51
Halis Yücel <isaycel2001@gmail.com>
2023-09-26 23:31:23 +0300

    chore: migrate to yarn

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,15 +1,19 @@
-import { Box } from '@monorepo/ui/core';
-import { Icon2fa, IconAbacus, Icon360View } from '@monorepo/icons';
+import { Text } from '@monorepo/ui/core';
+import { IconLogout } from '@monorepo/icons';
 
 export default function App() {
 	return (
 		<div>
-			<Box as="a" href="https://example.com">
-				example.com
-			</Box>
-			<Icon2fa />
-			<IconAbacus />
-			<Icon360View />
+			<Text
+				as="a"
+				size="xl"
+				decoration="none"
+				color="red-400"
+				href="https://example.com"
+				target="_blank"
+			>
+				example.com <IconLogout />
+			</Text>
 		</div>
 	);
 }
