6f44ae820908b6419844629dd5a0f904a1be672c
Halis Yücel <isaycel2001@gmail.com>
2023-09-28 01:25:02 +0300

    fix(eslint-config): fix eslint tsconfig configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,52 +1,46 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"airbnb",
-		"airbnb/hooks",
-		"airbnb-typescript",
-		"plugin:react/recommended",
-		"plugin:import/recommended",
-		"plugin:jsx-a11y/recommended",
-		"plugin:storybook/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier",
-	],
-	"overrides": [
-		{
-			"files": ["*.test.ts", "*.test.tsx"],
-			"rules": {
-				"@typescript-eslint/no-use-before-define": "off",
-			}
-		},
-		{
-			"files": ["index.ts"],
-			"rules": {
-				"no-restricted-exports": "off",
-				"import/prefer-default-export": "off",
-			}
-		}
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module",
-		"warnOnUnsupportedTypeScriptVersion": false,
-	},
-	"plugins": [
-		"react",
-		"@typescript-eslint",
-		"prettier",
-	],
-	"rules": {
-		"prettier/prettier": "error",
-		"react/react-in-jsx-scope": "off",
-		"react/require-default-props": "off",
-		"react/jsx-props-no-spreading": "off",
-		"import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:storybook/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      files: ["*.test.ts", "*.test.tsx"],
+      rules: {
+        "@typescript-eslint/no-use-before-define": "off",
+      },
+    },
+    {
+      files: ["index.ts"],
+      rules: {
+        "no-restricted-exports": "off",
+        "import/prefer-default-export": "off",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "react/jsx-props-no-spreading": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+  },
 };
